0c7abe79f58fcb6d606d6ff1de543ada6461148d
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-11 11:25:56 +0100

    feat: platform cards

---
+++ src/components/OSCard/OScard.jsx
@@ -0,0 +1,18 @@
+import {DownloadButton} from "../DownloadButton/DownloadButton";
+
+const OScard = (props) => {
+    return (
+        <div className="flex flex-col border-2 border-indigo-400 rounded-md p-10
+                        hover:bg-indigo-200 place-items-center gap-y-5">
+            <div className="flex flex-auto">
+                { props.os.icon }
+            </div>
+            <span className="flex flex-auto">
+                { props.os.name }
+            </span>
+            <DownloadButton/>
+        </div>
+    );
+}
+
+export { OScard };
