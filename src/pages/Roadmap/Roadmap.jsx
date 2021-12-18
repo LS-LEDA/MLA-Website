1f883407288d77bb452b5ae944cfa38a5ca0d176
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-18 14:53:20 +0100

    refactor: map transparent background

--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -2,8 +2,22 @@ import {Map} from "../../components/Map/Map";
 
 const Roadmap = () => {
     return (
-        <section id="roadmap">
-            <Map/>
+        <section id="roadmap" className="flex w-full bg-indigo-200">
+            <div className="flex flex-col w-1/2 align-middle m-auto pl-20">
+                <h1 className="font-black text-9xl">
+                    MLA
+                </h1>
+                <h1 className="font-black text-9xl">
+                    Development
+                </h1>
+                <h1 className="font-black text-9xl">
+                    Roadmap
+                </h1>
+            </div>
+
+            <div className="flex w-1/2">
+                <Map/>
+            </div>
         </section>
     );
 }
