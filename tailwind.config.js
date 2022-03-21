ff62e90b4a779f3f6a458863d807a084d6b7deb1
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-21 18:39:50 +0000

    chore: update tailwind & autoprefixer & postcss dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
