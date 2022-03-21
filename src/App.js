0607fd6493071026b9176ffbeed7272bc9520cfa
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-21 20:05:22 +0000

    feat: add navigation bar to webpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Features} from "./pages/Features/Features";
 import {HeroBanner} from "./pages/HeroBanner/HeroBanner";
 import {Roadmap} from "./pages/Roadmap/Roadmap";
+import NavBar from "./components/NavigationBar/NavBar";
 
 function App() {
     return (
         <div className="content-center">
+            <NavBar/>
             <HeroBanner/>
             <Features/>
             <Roadmap/>
