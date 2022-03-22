970b5da35cb239b31504396cb7d549b03e6f1f5a
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-22 16:23:23 +0000

    feat: load theme on website startup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import {Features} from "./pages/Features/Features";
 import {HeroBanner} from "./pages/HeroBanner/HeroBanner";
 import {Roadmap} from "./pages/Roadmap/Roadmap";
 import NavBar from "./components/NavigationBar/NavBar";
+import {useEffect} from "react";
 
 function App() {
+    // Apply dark / light theme on app mount
+    useEffect( () => {
+        if (localStorage.theme === 'dark' || !('theme' in localStorage)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    },
+        []
+    );
+
     return (
         <div className="content-center">
             <NavBar/>
