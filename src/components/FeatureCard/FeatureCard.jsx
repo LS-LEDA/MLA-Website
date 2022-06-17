90ba5a4d1f5ec2c1bc456c1529fbb6093c1c089a
JiahuiChen99 <Jiahui1@hotmail.es>
2022-06-17 19:48:00 +0100

    refactor: featurecard layout

--- src/components/FeatureCard/FeatureCard.jsx
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -1,18 +1,19 @@
 const FeatureCard = (props) => {
     return (
-        <div className="flex flex-col w-full h-auto hover:bg-primary_variant p-5 rounded-lg hover:cursor-pointer"
+        <div className="flex w-full h-auto hover:bg-primary p-5 rounded-lg hover:cursor-pointer space-x-6"
              onClick={() => { props.select(props.id) }}
         >
-            <div className="flex gap-x-4 items-center">
+            <div className="flex">
                 {/* Feature card icon */}
                 <div className="flex bg-primary rounded-md w-12 h-12 place-items-center justify-center">
                     { props.feature.icon }
                 </div>
-                {/* Feature card title */}
-                <div className="font-bold text-xl flex-1"> { props.feature.title } </div>
             </div>
+
             {/* Feature card content */}
-            <div className="flex flex-col pl-16">
+            <div className="flex flex-col w-full">
+                {/* Feature card title */}
+                <div className="flex font-bold text-xl"> { props.feature.title } </div>
                 <p className="max-w-full"> { props.feature.description } </p>
                 <span className="place-self-end font-bold text-sm mt-4">
                     <a href="#">
