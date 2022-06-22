54f776d0077fa7066d5b4a9b1890179c24dc6d19
JiahuiChen99 <Jiahui1@hotmail.es>
2022-06-22 15:49:38 +0200

    feat: HeroBanner responsive

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,6 +1,7 @@
 const IconButton = (props) => {
     return(
-        <button className="flex space-x-2 bg-primary hover:bg-primary_variant dark:bg-dark_primary dark:hover:bg-dark_primary_variant rounded-md px-5 py-2 place-items-center dark:text-dark_text">
+        <button className="flex justify-center bg-primary hover:bg-primary_variant dark:bg-dark_primary
+        dark:hover:bg-dark_primary_variant rounded-md px-5 py-2.5 place-items-center dark:text-dark_text">
             { props.icon }
             { props.download
                 ? <a href={props.link} download={props.download}>{props.text}</a>
--- src/pages/HeroBanner/HeroBanner.jsx
+++ src/pages/HeroBanner/HeroBanner.jsx
@@ -64,13 +64,13 @@ const HeroBanner = (props) => {
     }, []);
 
     return (
-        <section className="flex flex-col w-full h-screen place-items-center dark:bg-dark_bg px-10 md:px-20 lg:px-32 xl:40 2xl:px-44">
-            <div className="flex w-full h-[90vh] place-items-center">
-                <div className="flex flex-col w-1/2 h-full place-items-center justify-center">
+        <section className="flex flex-col w-full h-screen place-items-center dark:bg-dark_bg px-10 pt-32 md:px-20 lg:px-32 xl:px-40 2xl:px-44 xl:pt-0">
+            <div className="flex flex-col xl:flex-row w-full h-[90vh] place-items-center">
+                <div className="flex flex-col w-full xl:w-1/2 h-full place-items-center justify-center">
                     {/* Header */}
-                    <h1 className="flex text-center w-auto text-8xl font-bold dark:text-dark_text"> Moodle Learning Analytics </h1>
+                    <h1 className="flex text-center w-auto text-4xl sm:text-6xl xl:text-8xl font-bold dark:text-dark_text"> Moodle Learning Analytics </h1>
                     {/* Download buttons */}
-                    <div className="flex w-fit h-fit space-x-5 mt-20">
+                    <div className="flex flex-col xl:flex-row xl:justify-center w-full sm:w-4/6 h-auto space-y-2 xl:space-y-0 xl:space-x-2 mt-10 xl:mt-20">
                         {
                             os.map( (system, index) => {
                                 return <IconButton text={system.name} icon={system.icon} link={system.download_url} download={false} key={index}/>
@@ -84,7 +84,7 @@ const HeroBanner = (props) => {
                     </a>
                 </div>
 
-                <img className="flex w-1/2 h-fit"
+                <img className="flex w-full xl:w-1/2 h-fit"
                      src="https://raw.githubusercontent.com/LS-LEDA/MLA/main/resources/demo/demo_dashboard.png"
                      alt="Moodle Learning Analytics Dashboard"/>
             </div>
