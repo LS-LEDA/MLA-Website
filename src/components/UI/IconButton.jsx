bc9da5472e3b34f3dd0668d08814b5be19ff27ec
JiahuiChen99 <Jiahui1@hotmail.es>
2022-07-05 20:25:50 +0100

    feat: icon button icon text separation

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,7 +1,8 @@
 const IconButton = (props) => {
     return(
         <button className="flex justify-center bg-primary hover:bg-primary_variant dark:bg-dark_primary
-        dark:hover:bg-dark_primary_variant rounded-md px-5 py-2.5 place-items-center dark:text-dark_text">
+        dark:hover:bg-dark_primary_variant rounded-md px-5 py-2.5 place-items-center dark:text-dark_text
+        space-x-2">
             { props.icon }
             { props.download
                 ? <a href={props.link} download={props.download}>{props.text}</a>
