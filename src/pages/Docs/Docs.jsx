e3d7637074659e191d9ca3cb2c6ba79f2872b832
JiahuiChen99 <Jiahui1@hotmail.es>
2022-07-06 20:30:08 +0100

    feat: docs page

---
+++ src/pages/Docs/Docs.jsx
@@ -0,0 +1,12 @@
+import {Construction} from "../../components/Construction/Construction";
+
+const Docs = () => {
+    return (
+        <div className="flex w-full h-[100vh] dark:bg-dark_bg px-10 pt-32 md:px-20 lg:px-32 xl:px-40 2xl:px-44 xl:pt-0
+                        place-items-center justify-center">
+            <Construction/>
+        </div>
+    )
+}
+
+export { Docs };
